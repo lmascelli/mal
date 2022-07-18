cab73a10e35b98b6985bb4110265a4883dcb3d04
lmascelli <40148338+lmascelli@users.noreply.github.com>
2022-07-18 18:54:50 +0200

    Update mylisp.h

--- step1/mylisp.h
+++ step1/mylisp.h
@@ -1,35 +1,73 @@
-#pragma once
-#include <string>
-#include <string_view>
-#include <vector>
-using std::string, std::vector;
+#include "mylisp.h"
 
 namespace mylisp {
-extern bool running;
+bool running = true;
 
-string READ(string input);
-string EVAL(string input);
-string PRINT(string input);
-string rep(string input);
+string READ(string input) {
+  Reader r(input);
+  return r.next().data();
+}
 
-class Reader {
-  public:
-    string next();
-    string peek();
-};
+string EVAL(string input) {
+  if (input.find(EOF) != string::npos)
+    running = false;
+  return input;
+}
 
-Reader* read_str();
-void read_form(Reader* reader);
+string PRINT(string input) { return input; }
 
-class Tokenizer {
-  public:
-    Tokenizer(string &input);
+string rep(string input) { return PRINT(EVAL(READ(input))); }
 
-  private:
-    string &m_imput;
+Reader::Reader(string &input) : _input(input) {}
 
-};
+string_view Reader::next() {
+  string_view view(_input);
+  while (m_index < view.length()) {
+    char ch = view.at(m_index);
+    switch (ch) {
+    case '~':
+      if (view.at(m_index + 1) == '@')
+        current = view.substr(m_index, 2);
+        return current;
+    case '[':
+    case ']':
+    case '{':
+    case '}':
+    case '(':
+    case ')':
+    case '\'':
+    case '`':
+    case '^':
+    case '@':
+      current = view.substr(m_index, 1);
+      return current;
+    case '"': {
+      size_t cl = 1;
+      do {
+        if (view.at(m_index + cl) == '"' and view.at(m_index + cl - 1) != '\\')
+          break;
+        cl++;
+      } while (m_index + cl < view.length());
+      return view.substr(m_index, cl);
+    }
+    case '\\':
+    case '.':
+    case ';':
+    case '\r':
+    case '\t':
+    case '\n':
+    case EOF:
+      current = view.substr(m_index, 1);
+      return current;
+    }
+    m_index++;
+  }
+  current = view.substr(m_index);
+  return current;
+}
 
-vector<string> tokenize(string text);
+string_view Reader::peek(){
+  return current;
+}
 
 } // namespace mylisp
